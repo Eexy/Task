12f922e9b7a18669e1a77a24ec83891473d9e6b7
unknown <eexdhe@gmail.com>
2021-01-29 20:49:17 +0100

    install handlebars as template engine

--- src/server/app.js
+++ src/server/app.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
 const PORT = 3000;
 
-// path to public directory
+// set views and partials path
+const viewsPath = path.join(__dirname, '../public/views');
+
+// set template engine
+app.engine('.hbs', hbs({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+app.set('views', viewsPath);
+
+// set path to public directory
 const publicDirectory = path.join(__dirname, '../public');
-console.log(publicDirectory);
 
 // configure public file
 app.use(express.static(publicDirectory));
 
 app.get('', (req, res) => {
-  res.sendFile(path.join(publicDirectory, 'pages/index.html'));
+  res.render('index', { title: 'homepage' });
 });
 
 app.listen(PORT, () => {
