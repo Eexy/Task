d7c839587e16af1eda8fb08b65b4507ea845f382
unknown <eexdhe@gmail.com>
2021-01-31 21:58:11 +0100

    upgrade user model

--- src/server/database/models/userModel.js
+++ src/server/database/models/userModel.js
@@ -1,8 +1,22 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const schema = new mongoose.Schema({
-  email: 'string',
-  pwd: 'string',
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    validate(val) {
+      if (!validator.isEmail(val)) {
+        throw new Error('You must provide an email address');
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
 });
 
 const User = mongoose.model('User', schema);
