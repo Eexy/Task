9472808c978ce7202db4c28a6dcec28c2d65127c
unknown <eexdhe@gmail.com>
2021-02-10 18:41:39 +0100

    create modal & overlay style

---
+++ src/public/js/dashboard.js
@@ -0,0 +1,28 @@
+import Task from 'Task.js';
+const taskList = document.querySelector('tasks-list');
+const taskTitle = document.querySelector('#task-title');
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  createTask(taskTitle.value);
+});
+
+async function createTask(title) {
+  const params = {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ title }),
+  };
+
+  const res = await fetch('http://localhost:3000/tasks', params);
+  const json = await res.json();
+
+  if (!json.error) {
+    const task = new Task(json._id, json.title, json.completed, taskList);
+    taskList.add(task);
+  }
+}
