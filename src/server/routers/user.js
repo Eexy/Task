4e46b493502a88ce550e13373daf023ea5db4ea9
unknown <eexdhe@gmail.com>
2021-02-11 18:38:56 +0100

    final test 5 inshallah

--- src/server/routers/user.js
+++ src/server/routers/user.js
@@ -51,16 +51,18 @@ router.get('/users/signup', (req, res) => {
 
 router.post('/users/signup', async (req, res) => {
   let user = null;
-   User.findOne({ email: req.body.email })
-   .then((res) => {
-     res.send({ error: 'User already exist' });
-   })
-   .catch((e) => {
-    user = new User(req.body);
-    const token = await user.generateAuthToken();
-    res.cookie('jwt', token, { httpOnly: true, maxAge: 9000000000 });
-    return res.send(user);
-   });
+  try {
+    user = await User.findOne({ email: req.body.email });
+  } catch (e) {}
+
+  if (user != null) {
+    return res.send({ error: 'User already exist' });
+  }
+
+  user = new User(req.body);
+  const token = await user.generateAuthToken();
+  res.cookie('jwt', token, { httpOnly: true, maxAge: 9000000000 });
+  res.send(user);
 });
 
 router.get('/dashboard', auth, (req, res) => {
