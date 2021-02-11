a4acbe6130ffde08c74272c568988fba34f1cb33
unknown <eexdhe@gmail.com>
2021-02-11 17:53:50 +0100

    fix typo in mongodb cluster url

--- src/server/database/db.js
+++ src/server/database/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(`mongodb+srv://taskapp:${process.env.CLUSTER}@kha.1znlw.mongodb.net/${process.send.DBNAME}?retryWrites=true&w=majority`, {
+mongoose.connect(`mongodb+srv://taskapp:${process.env.CLUSTER}@kha.1znlw.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
