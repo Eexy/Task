aeb3876f1093748f8cd9cdd5660325607546e720
unknown <eexdhe@gmail.com>
2021-02-11 11:33:15 +0100

    reconnect create tasks'form with backend

--- src/public/js/dashboard.js
+++ src/public/js/dashboard.js
@@ -1,12 +1,8 @@
-/* import Task from '../js/Task.js';
+import Task from './Task.js';
+
 const taskList = document.querySelector('tasks-list');
 const taskTitle = document.querySelector('#task-title');
-const form = document.querySelector('form');
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  createTask(taskTitle.value);
-});
+const form = document.querySelector('#createTaskForm');
 
 async function createTask(title) {
   const params = {
@@ -25,4 +21,12 @@ async function createTask(title) {
     const task = new Task(json._id, json.title, json.completed, taskList);
     taskList.add(task);
   }
-} */
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  createTask(taskTitle.value);
+
+  const modal = document.querySelector('#create-task-modal');
+  closeModal('#'+modal.getAttribute('id'));
+});
